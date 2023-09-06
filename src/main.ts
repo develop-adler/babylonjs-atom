3ccbf2d1979435bbfb9cb5f80384af89051a3a34
gavinadler <giavi.quach@adler.cx>
2023-09-06 09:45:49 +0700

    enable shadow for character mesh + refactor + remove unnecessary ground

--- src/main.ts
+++ src/main.ts
@@ -15,39 +15,36 @@ import ClassicRoom from "./components/Atoms/ClassicRoom";
 declare function HavokPhysics(): any;
 
 class App {
-    private canvas: HTMLCanvasElement;
-    private engine: BABYLON.Engine;
-    private scene: BABYLON.Scene;
-    private havok!: HavokPhysicsWithBindings;
-    private camera!: BABYLON.ArcRotateCamera | BABYLON.UniversalCamera;
-    private character!: Character;
-    private characterController?: CharacterController;
-    private joystick: Joystick;
+    private _canvas: HTMLCanvasElement;
+    private _engine: BABYLON.Engine;
+    private _scene: BABYLON.Scene;
+    private _havok!: HavokPhysicsWithBindings;
+    private _camera!: BABYLON.ArcRotateCamera | BABYLON.UniversalCamera;
+    private _character!: Character;
+    private _characterController?: CharacterController;
+    private _joystick: Joystick;
     // private appState: 0 | 1 | 2 = 1;
 
     private isThirdperson: boolean = false;
 
-    private ground: BABYLON.Mesh;
-
     constructor() {
         // this.initHUD();
 
-        this.canvas = document.createElement("canvas");
-        this.canvas.style.width = "100%";
-        this.canvas.style.height = "100%";
-        this.canvas.id = "babylonCanvas";
-        document.getElementById("app")!.appendChild(this.canvas);
+        this._canvas = document.createElement("canvas");
+        this._canvas.style.width = "100%";
+        this._canvas.style.height = "100%";
+        this._canvas.id = "babylonCanvas";
+        document.getElementById("app")!.appendChild(this._canvas);
 
-        this.joystick = new Joystick();
+        this._joystick = new Joystick();
 
         // initialize babylon scene and engine
-        this.engine = new BABYLON.Engine(this.canvas, true);
+        this._engine = new BABYLON.Engine(this._canvas, true);
 
         // show loading screen
-        this.engine.displayLoadingUI();
+        this._engine.displayLoadingUI();
 
-        this.scene = new BABYLON.Scene(this.engine);
-        this.ground = new BABYLON.Mesh("ground", this.scene);
+        this._scene = new BABYLON.Scene(this._engine);
 
         // wait until scene has physics then create scene
         this.initScene().then(async () => {
@@ -57,12 +54,12 @@ class App {
             // thirperson controller mode as default mode
             this.initThirdPersonController();
             await this.initCharacterAsync();
-            this.characterController = new CharacterController(
-                this.character!.root as BABYLON.Mesh,
-                this.character!.physicsBody as BABYLON.PhysicsBody,
-                this.camera as BABYLON.ArcRotateCamera,
-                this.scene,
-                this.joystick,
+            this._characterController = new CharacterController(
+                this._character!.root as BABYLON.Mesh,
+                this._character!.physicsBody as BABYLON.PhysicsBody,
+                this._camera as BABYLON.ArcRotateCamera,
+                this._scene,
+                this._joystick,
             );
 
             this.createLight();
@@ -70,18 +67,18 @@ class App {
             this.initControls();
 
             // hide loading screen
-            this.engine.hideLoadingUI();
+            this._engine.hideLoadingUI();
 
-            this.engine.runRenderLoop(() => {
-                if (this.scene) this.scene.render();
+            this._engine.runRenderLoop(() => {
+                if (this._scene) this._scene.render();
             });
 
             // the canvas/window resize event handler
-            const handleResize = () => this.engine.resize();
+            const handleResize = () => this._engine.resize();
             window.addEventListener("resize", handleResize);
 
             // remove event listener
-            this.scene.onDispose = () => {
+            this._scene.onDispose = () => {
                 window.removeEventListener("resize", handleResize);
 
                 this.dispose();
@@ -92,56 +89,56 @@ class App {
     async initScene(): Promise<void> {
         const envMapTexture = BABYLON.CubeTexture.CreateFromPrefilteredData(
             "/envMap/sky.env",
-            this.scene,
+            this._scene,
         );
-        this.scene.environmentTexture = envMapTexture;
-        this.scene.createDefaultSkybox(envMapTexture, true);
-        this.scene.environmentIntensity = 0.5;
+        this._scene.environmentTexture = envMapTexture;
+        this._scene.createDefaultSkybox(envMapTexture, true);
+        this._scene.environmentIntensity = 0.5;
 
         // Enable physics
         const gravityVector = new BABYLON.Vector3(0, -19.62, 0);
-        this.havok = await HavokPhysics();
+        this._havok = await HavokPhysics();
         // pass the engine to the plugin
-        const havokPlugin = new BABYLON.HavokPlugin(true, this.havok);
+        const havokPlugin = new BABYLON.HavokPlugin(true, this._havok);
         // enable physics in the scene with a gravity
-        this.scene.enablePhysics(gravityVector, havokPlugin);
+        this._scene.enablePhysics(gravityVector, havokPlugin);
 
-        this.scene.collisionsEnabled = true;
+        this._scene.collisionsEnabled = true;
     }
 
     initFirstPersonController(pointerLock: boolean = false): void {
         this.resetCamera();
 
-        this.camera = new BABYLON.UniversalCamera(
+        this._camera = new BABYLON.UniversalCamera(
             "camera",
             new BABYLON.Vector3(0, 2.5, -2),
-            this.scene,
+            this._scene,
         );
-        this.camera.attachControl();
+        this._camera.attachControl();
 
         if (pointerLock) {
-            this.engine.enterPointerlock();
-            this.scene.onPointerDown = e => {
+            this._engine.enterPointerlock();
+            this._scene.onPointerDown = e => {
                 // left click
-                if (e.button === 0) this.engine.enterPointerlock();
+                if (e.button === 0) this._engine.enterPointerlock();
             };
         } else {
-            this.engine.exitPointerlock();
-            this.scene.onPointerDown = undefined;
+            this._engine.exitPointerlock();
+            this._scene.onPointerDown = undefined;
         }
 
-        this.camera.applyGravity = true; // apply gravity to the camera
-        this.camera.checkCollisions = true; // prevent walking through walls
-        this.camera.ellipsoid = new BABYLON.Vector3(1, 1.25, 1); // collision box
-        this.camera.speed = 1; // walking speed
-        this.camera.inertia = 0.5; // reduce slipping
-        this.camera.minZ = 0.1; // prevent clipping
-        this.camera.angularSensibility = 1200; // mouse sensitivity: higher value = less sensitive
+        this._camera.applyGravity = true; // apply gravity to the camera
+        this._camera.checkCollisions = true; // prevent walking through walls
+        this._camera.ellipsoid = new BABYLON.Vector3(1, 1.25, 1); // collision box
+        this._camera.speed = 1; // walking speed
+        this._camera.inertia = 0.5; // reduce slipping
+        this._camera.minZ = 0.1; // prevent clipping
+        this._camera.angularSensibility = 1200; // mouse sensitivity: higher value = less sensitive
 
-        this.camera.keysUp.push(87); // W
-        this.camera.keysLeft.push(65); // A
-        this.camera.keysDown.push(83); // S
-        this.camera.keysRight.push(68); // D
+        this._camera.keysUp.push(87); // W
+        this._camera.keysLeft.push(65); // A
+        this._camera.keysDown.push(83); // S
+        this._camera.keysRight.push(68); // D
 
         this.isThirdperson = false;
     }
@@ -150,64 +147,64 @@ class App {
         if (this.isThirdperson) return;
         this.resetCamera();
 
-        this.camera = new BABYLON.ArcRotateCamera(
+        this._camera = new BABYLON.ArcRotateCamera(
             "camera",
             -Math.PI * 0.5,
             Math.PI * 0.5,
             5,
             new BABYLON.Vector3(0, 1.15, -2), // target
-            this.scene,
+            this._scene,
         );
 
-        this.camera.position = new BABYLON.Vector3(0, 3, -6);
+        this._camera.position = new BABYLON.Vector3(0, 3, -6);
 
         // This attaches the camera to the canvas
-        this.camera.attachControl(this.canvas, true);
+        this._camera.attachControl(this._canvas, true);
 
         // prevent clipping
-        this.camera.minZ = 0.1;
+        this._camera.minZ = 0.1;
 
-        this.camera.wheelPrecision = 100;
+        this._camera.wheelPrecision = 100;
 
         // camera min distance and max distance
-        this.camera.lowerRadiusLimit = 0.5;
-        this.camera.upperRadiusLimit = 10;
+        this._camera.lowerRadiusLimit = 0.5;
+        this._camera.upperRadiusLimit = 10;
 
         //  lower rotation sensitivity, higher value = less sensitive
-        this.camera.angularSensibilityX = 2000;
-        this.camera.angularSensibilityY = 2000;
+        this._camera.angularSensibilityX = 2000;
+        this._camera.angularSensibilityY = 2000;
 
         // disable rotation using keyboard arrow key
-        this.camera.keysUp = [];
-        this.camera.keysDown = [];
-        this.camera.keysLeft = [];
-        this.camera.keysRight = [];
+        this._camera.keysUp = [];
+        this._camera.keysDown = [];
+        this._camera.keysLeft = [];
+        this._camera.keysRight = [];
 
         this.isThirdperson = true;
     }
 
     initControls(): void {
         // Keyboard input
-        this.scene.onKeyboardObservable.add(async kbInfo => {
+        this._scene.onKeyboardObservable.add(async kbInfo => {
             if (kbInfo.type === BABYLON.KeyboardEventTypes.KEYDOWN) {
                 switch (kbInfo.event.key.toLowerCase().trim()) {
                     case "i":
-                        if (this.scene.debugLayer.isVisible()) {
-                            this.scene.debugLayer.hide();
+                        if (this._scene.debugLayer.isVisible()) {
+                            this._scene.debugLayer.hide();
                         } else {
-                            this.scene.debugLayer.show();
+                            this._scene.debugLayer.show();
                         }
                         break;
                     case "1":
                         // switch to first person controller (with pointer lock) by pressing 1
                         this.stopCharacterController();
-                        this.character?.hide();
+                        this._character?.hide();
                         this.initFirstPersonController(true);
                         break;
                     case "2":
                         // switch to first person controller (without pointer lock) by pressing 2
                         this.stopCharacterController();
-                        this.character?.hide();
+                        this._character?.hide();
                         this.initFirstPersonController(false);
                         break;
                     case "3":
@@ -215,30 +212,30 @@ class App {
                         // switch to third person controller by pressing 3
                         this.initThirdPersonController();
 
-                        if (!this.character) {
+                        if (!this._character) {
                             await this.initCharacterAsync();
                         } else {
-                            this.character.show();
+                            this._character.show();
                         }
 
-                        if (!this.characterController) {
-                            this.characterController = new CharacterController(
-                                this.character!.root as BABYLON.Mesh,
-                                this.character!.physicsBody as BABYLON.PhysicsBody,
-                                this.camera as BABYLON.ArcRotateCamera,
-                                this.scene,
-                                this.joystick,
+                        if (!this._characterController) {
+                            this._characterController = new CharacterController(
+                                this._character!.root as BABYLON.Mesh,
+                                this._character!.physicsBody as BABYLON.PhysicsBody,
+                                this._camera as BABYLON.ArcRotateCamera,
+                                this._scene,
+                                this._joystick,
                             );
                         } else {
-                            this.characterController.start();
+                            this._characterController.start();
                         }
                         break;
                 }
             }
         });
 
-        // phone input for ball
-        const hammerManager = new Hammer.Manager(this.canvas);
+        // phone input
+        const hammerManager = new Hammer.Manager(this._canvas);
 
         // create swipe gesture recognizer and add recognizer to manager
         const Swipe = new Hammer.Swipe();
@@ -247,7 +244,6 @@ class App {
         hammerManager.get("swipe").set({ direction: Hammer.DIRECTION_ALL });
         hammerManager.on("swipe", (e: any) => {
             switch (e.direction) {
-                // swipe up to throw ball
                 case Hammer.DIRECTION_UP:
                     break;
                 case Hammer.DIRECTION_LEFT:
@@ -263,7 +259,7 @@ class App {
         const hemiLight = new BABYLON.HemisphericLight(
             "hemiLight",
             new BABYLON.Vector3(0, 1, 0),
-            this.scene,
+            this._scene,
         );
 
         // dim light a small amount
@@ -272,7 +268,7 @@ class App {
         const dirLight = new BABYLON.DirectionalLight(
             "dirLight",
             new BABYLON.Vector3(-0.5, -1, -0.5),
-            this.scene,
+            this._scene,
         );
 
         dirLight.position = new BABYLON.Vector3(30, 20, -10);
@@ -281,7 +277,7 @@ class App {
         dirLight.shadowMinZ = 10;
         dirLight.shadowMaxZ = 60;
 
-        // this.createLightGizmo(dirLight);
+        this.createLightGizmo(dirLight);
 
         // Shadows
         const shadowGenerator = new BABYLON.ShadowGenerator(2048, dirLight);
@@ -294,22 +290,10 @@ class App {
         // low quality for better performance
         // shadowGenerator.filteringQuality = ShadowGenerator.QUALITY_LOW;
 
-        // this.character?.meshes.forEach(mesh => {
-        //     mesh.receiveShadows = true;
-        //     shadowGenerator.addShadowCaster(mesh);
-        // });
-
-        // this.bowlingAlleyObjects.ball.receiveShadows = true;
-        // shadowGenerator.addShadowCaster(this.bowlingAlleyObjects.ball);
-
-        // this.bowlingAlleyObjects.pinMeshes.forEach(mesh => {
-        //     mesh.receiveShadows = true;
-        //     shadowGenerator.addShadowCaster(mesh);
-        // });
-        // this.bowlingAlleyObjects.facility.forEach(mesh => {
-        //     mesh.receiveShadows = true;
-        //     shadowGenerator.addShadowCaster(mesh);
-        // });
+        this._character?.meshes.forEach(mesh => {
+            mesh.receiveShadows = true;
+            shadowGenerator.addShadowCaster(mesh);
+        });
     }
 
     createLightGizmo(customLight: BABYLON.Light): void {
@@ -317,7 +301,7 @@ class App {
         lightGizmo.scaleRatio = 2;
         lightGizmo.light = customLight;
 
-        const gizmoManager = new BABYLON.GizmoManager(this.scene);
+        const gizmoManager = new BABYLON.GizmoManager(this._scene);
         gizmoManager.positionGizmoEnabled = true;
         gizmoManager.rotationGizmoEnabled = true;
         gizmoManager.usePointerToAttachGizmos = false;
@@ -325,69 +309,56 @@ class App {
     }
 
     resetCamera(): void {
-        this.scene.removeCamera(this.camera);
-        if (this.camera instanceof BABYLON.ArcRotateCamera) {
-            this.camera.dispose();
+        this._scene.removeCamera(this._camera);
+        if (this._camera instanceof BABYLON.ArcRotateCamera) {
+            this._camera.dispose();
         }
 
-        this.engine.exitPointerlock();
-        this.scene.onPointerDown = undefined;
-
-        if (this.ground) {
-            this.scene.removeMesh(this.ground);
-
-            if (this.ground?.material) {
-                this.scene.removeMaterial(this.ground.material);
-                this.ground.material!.dispose();
-            }
-
-            this.ground?.dispose();
-            this.ground = null!;
-        }
+        this._engine.exitPointerlock();
+        this._scene.onPointerDown = undefined;
     }
 
     createAtom(): void {
-        const classicRoom = new ClassicRoom(this.scene);
+        const classicRoom = new ClassicRoom(this._scene);
         console.log(classicRoom);
     }
 
     initCharacter(): void {
-        if (this.character) return;
-        this.character = new Character(this.scene);
-        this.character.init();
+        if (this._character) return;
+        this._character = new Character(this._scene);
+        this._character.init();
     }
 
     async initCharacterAsync(): Promise<void> {
-        if (this.character) return;
-        this.character = new Character(this.scene);
-        await this.character.init();
+        if (this._character) return;
+        this._character = new Character(this._scene);
+        await this._character.init();
     }
 
     disposeCharacter(): void {
-        this.character?.dispose();
-        this.character = null!;
+        this._character?.dispose();
+        this._character = null!;
     }
 
     stopCharacterController(): void {
-        this.characterController?.dispose();
-        this.characterController = null!;
+        this._characterController?.dispose();
+        this._characterController = null!;
     }
 
     dispose(): void {
         // dispose cameras
-        this.scene.cameras.forEach(camera => {
+        this._scene.cameras.forEach(camera => {
             camera.dispose();
         });
 
         // dispose character and character controller
         this.disposeCharacter();
         this.stopCharacterController();
-        this.joystick.dispose();
+        this._joystick.dispose();
 
-        this.engine.stopRenderLoop();
-        this.scene.dispose();
-        this.engine.dispose();
-
+        this._engine.stopRenderLoop();
+        this._scene.dispose();
+        this._engine.dispose();
     }
 }
 
